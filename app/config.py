bcd9002c82e4593261baf2fc4e207fd7d3114223
Valentino Belmonte <42761264+freshSauce@users.noreply.github.com>
2023-12-05 21:30:58 -0600

    Update config.py

--- app/config.py
+++ app/config.py
@@ -10,24 +10,18 @@ class Config:
     # Clave secreta para proteger las sesiones y cookies
     SECRET_KEY = SECRET_KEY
 
-    # Resto de tus configuraciones...
-
 
 class DevelopmentConfig(Config):
     # Configuraciones específicas para el entorno de desarrollo
 
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
-    # Resto de tus configuraciones de desarrollo...
-
 
 class ProductionConfig(Config):
     # Configuraciones específicas para el entorno de producción
 
     DEBUG = False
     SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
-    # Resto de tus configuraciones de producción...
-
 
 # Configuración de la aplicación Flask
 app_config = {"development": DevelopmentConfig, "production": ProductionConfig}
